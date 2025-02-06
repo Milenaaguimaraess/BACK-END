b447c87fcb678d8e32ac214162fea240beb43a15
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-02-06 16:02:13 -0300

    Banco de dados
    aula sobre tabelas

---
+++ BancoDeDados/Correçao atividade 1.sql
@@ -0,0 +1,7 @@
+insert into pessoas(nome,rg,cpf,idade, saldo) values("Xandao",1111111,00000000000,34,0);
+insert into pessoas(nome,rg,cpf,idade, saldo) values("fafa de belem",0111111,00000000010,54,600);
+insert into pessoas(nome,rg,cpf,idade, saldo) values("Carla Perez",1181111,00690000000,22,202);
+insert into pessoas(nome,rg,cpf,idade, saldo) values("Xuxa",1111611,00000030000,60,770.8);
+insert into pessoas(nome,rg,cpf,idade, saldo) values("Evaristo",1311111,00000022000,74,400.9);
+
+select * from pessoas;
