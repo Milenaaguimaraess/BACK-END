be0ab9a41b990e982f64ec517bd379b049b0f647
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-02-18 17:44:30 -0300

    Join - inner (left e right)

---
+++ BancoDeDados/atividades/Atividade8_180225.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE atividade8;
+USE atividade8;
+
+CREATE TABLE clientes(
+id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100)
+);
+
+CREATE TABLE pedidos(
+id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+data_pedido VARCHAR(20),
+id_cliente INT NOT NULL,
+foreign key (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+INSERT INTO clientes (nome) values ("Carlos");
+INSERT INTO pedidos (data_pedido,id_cliente) values ("25/01/2019",1);
+INSERT INTO pedidos (data_pedido,id_cliente) values ("28/01/2019",1);
+INSERT INTO pedidos (data_pedido,id_cliente) values ("02/02/2019",1);
+
+INSERT INTO clientes (nome) values ("Ana");
+INSERT INTO pedidos (data_pedido,id_cliente) values ("15/02/2019",2);
+
+INSERT INTO clientes (nome) values ("Bruna");
+
+INSERT INTO clientes (nome) values ("Jéssica");
+
+INSERT INTO clientes (nome) values ("Victor");
+INSERT INTO pedidos (data_pedido,id_cliente) values ("25/02/2019",5);
+
+INSERT INTO clientes (nome) values ("Fernanda");
+INSERT INTO pedidos (data_pedido,id_cliente) values ("29/02/2019",6);
+INSERT INTO pedidos (data_pedido,id_cliente) values ("30/02/2019",6);
+select * from clientes;
+
+-- Liste os pedidos e os nomes dos clientes que fizeram esses pedidos.
+SELECT pedidos.id_pedido , clientes.nome FROM pedidos inner join clientes ON pedidos.id_cliente, = clientes.id_cliente;
+-- Liste todos os clientes e os pedidos que fizeram (mesmo que não tenham feito pedidos).
+SELECT clientes.nome , pedidos.id_pedido FROM clientes right join pedidos on pedidos.id_cliente = clientes.id_cliente;
---
+++ BancoDeDados/conteúdo_aula/Inner_RightLeft180225.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE JoinAula;
+USE JoinAula;
+
+CREATE TABLE endereco (
+id int primary key auto_increment,
+rua varchar(255),
+numero varchar(255),
+pessoa_id int not null,
+foreign key (pessoa_id) references pessoa(id)
+);
+-- ao ter uma coluna com id, essa coluna é a chave estrangeira. ex: pessoa_id
+
+create table pessoa (
+id int primary key auto_increment,
+nome VARCHAR (100),
+nacionalidade VARCHAR (5)
+);
+
+INSERT INTO pessoa (nome, nacionalidade) values ("Wanessa","AT");
+INSERT INTO pessoa (nome, nacionalidade) values ("Peter","FR");
+INSERT INTO endereco (rua,numero,pessoa_id) values ("Ceilândia","QNQ 14",1);
+select * from endereco;
+
+select pessoa.nome , endereco.rua from pessoa join endereco on pessoa.id = endereco.id;
+
+-- inner -> intersenção ou o que as duas colunas tem em comum
+-- left e right -> é quando quero mostrar mais dados de um lado ou do outro da intersençãoptimize
+-- right join -> junta as duas tabelas, priorizando a tabela da direita
+select pessoa.nome , endereco.rua from pessoa right join endereco on pessoa.id = endereco.id;
+select pessoa.nome , endereco.rua from endereco right join pessoa on pessoa.id = endereco.id;
+-- mesmo q a tabela tenha valores nulos, mas to inclinando o que eu quero pra um lado, vai aparecer o valor correspondente msm q seja nulo pq estou priorizando esse lado
+-- left junta as duas tabelas, priorizando a tabela da esquerda
+select pessoa.nome , endereco.rua from endereco left join pessoa on pessoa.id = endereco.id;
+
+use employees;
+select * from employees;
+select * from dept_emp;
+ -- Group by -> quero agrupar as informações
+ -- para usar o group by deve-se perguntar: o que eu quero, de onde, agrupar quem?
+ select gender , count(gender) from employees group by gender;
+ select gender as genero, count(gender) AS qtd_per_genero from employees group by gender;
+-- no código acima, mudei o nome das colunas e ainda usei o group by
+ select dept_no, count(dept_no) from dept_emp group by dept_no;
+ 
