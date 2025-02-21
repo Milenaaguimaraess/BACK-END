d8a12a46d34c910b7433d5a5a0e82b151185caa9
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-02-21 17:37:06 -0300

    Projeto 1 - Banco de Dados

---
+++ ConcenssionariaMJ_210225.sql
@@ -0,0 +1,4 @@
+INSERT INTO venda (id_carro, id_cliente, id_colaborador, dataDaVenda, horaDaVenda, quantidade) VALUES (1,1,1,"25/10/2021","12:24 PM", 2);
+INSERT INTO venda (id_carro, id_cliente, id_colaborador, dataDaVenda, horaDaVenda, quantidade) VALUES (2,2,2,"12/11/2021","15:25 PM", 3);
+INSERT INTO venda (id_carro, id_cliente, id_colaborador, dataDaVenda, horaDaVenda, quantidade) VALUES (3,3,3,"17/11/2021","12:24 PM", 2);
+INSERT INTO venda (id_carro, id_cliente, id_colaborador, dataDaVenda, horaDaVenda, quantidade) VALUES (1,1,1,"25/10/2021","12:24 PM", 2);
